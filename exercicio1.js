ac247464db037fcd81de5baa00d3668a4f78f18e
Pedro Henrique Barbosa <144858750+EuPedroBarbosa29@users.noreply.github.com>
2024-07-08 20:19:10 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,22 @@
+let calcularOsNumeros = alert("Vamos calcular")
+
+let numberOne = prompt("PRIMEIRO NUMERO")
+let numberTwo = prompt ("SEGUNDO NUMERO")
+
+numberOne = Number(numberOne)
+numberTwo = Number(numberTwo)
+
+
+const soma = numberOne + numberTwo
+const sub = numberOne - numberTwo
+const mult = numberOne * numberTwo
+const div = numberOne / numberTwo
+const rest = numberOne % numberTwo
+
+
+
+alert ( " soma " + soma )
+alert ( " subtração " + sub  )
+alert ( "multipliação " + mult  )
+alert ( " divisão " + div  )
+alert ( " resto da divisão " + rest )
